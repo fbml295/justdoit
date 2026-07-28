7456f2b105f9e56c6149ba63989e407a1f5e7b5b
fbml295 <fbml295@gmail.com>
2026-07-28 16:29:57 +0700

    Update google-config.js

--- js/google-config.js
+++ js/google-config.js
@@ -7,12 +7,12 @@ const GOOGLE_CONFIG = {
     //    https://console.cloud.google.com/apis/credentials
     //    Authorized JavaScript origins PHẢI khớp đúng địa chỉ bạn dùng để mở trang này,
     //    ví dụ: https://ten-tai-khoan-github.github.io
-    CLIENT_ID: 'DÁN_CLIENT_ID_CỦA_BẠN_VÀO_ĐÂY.apps.googleusercontent.com',
+    CLIENT_ID: '464267721766-jp1uii17bnju6248u1ei2416a8hbhvbh.apps.googleusercontent.com',
 
     // 2. ID của thư mục Google Drive chứa file Google Sheet dữ liệu (thư mục bạn sẽ
     //    cấp quyền truy cập cho mọi người dùng). Lấy từ URL khi mở thư mục đó trên
     //    Google Drive: https://drive.google.com/drive/folders/ĐÂY_LÀ_FOLDER_ID
-    DRIVE_FOLDER_ID: 'DÁN_FOLDER_ID_CỦA_BẠN_VÀO_ĐÂY',
+    DRIVE_FOLDER_ID: '1RG_AstKeBggmI2syG7sg7xajt6iJ4dPP',
 
     // 3. (Tuỳ chọn) Nếu biết chính xác Spreadsheet ID muốn dùng, điền vào đây để app
     //    khỏi phải tự tìm trong thư mục ở trên. Để trống ('') thì app sẽ tự tìm file
